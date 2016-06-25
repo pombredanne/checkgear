9fbb056902e45183f7398efb9a856f54b8355e0e
sou-komatsu <sou.komatsu@gmail.com>
2016-06-25 17:59:21 +0900

    fix packaging.

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
         name             = 'checkgear',
         version          = '0.0.1',
         description      = 'Library Vulnerability Checker from CVE.',
-        long_description = '[command]$checkgear'
+        long_description = '[command]$checkgear',
         classifiers      = [
                            'Topic :: Security',
-                           ]
+                           ],
         license          = 'MIT',
         author           = 'Sou Komatsu',
         author_email     = 'sou.komatsu@gmail.com',
         url              = 'https://github.com/sou-komatsu/checkgear',
-        keywords         = ['python','security','cve'].
+        keywords         = ['python','security','cve'],
         packages         = find_packages(),
         install_requires = [],
         entry_points ="""
